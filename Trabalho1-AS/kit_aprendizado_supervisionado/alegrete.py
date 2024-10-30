0f74be4e465b71d02fd4317d02d5f07106877cb1
Artur Santos <arturbernardogithub@gmail.com>
2024-10-30 18:09:10 -0300

    adicionando exerc 1 - regressão linear

--- Trabalho1-AS/kit_aprendizado_supervisionado/alegrete.py
+++ Trabalho1-AS/kit_aprendizado_supervisionado/alegrete.py
@@ -1,33 +1,31 @@
 import numpy as np
 
-
 def compute_mse(b, w, data):
     """
     Calcula o erro quadratico medio
     :param b: float - bias (intercepto da reta)
-    :param w: fwloat - peso (inclinacao da reta)
+    :param w: float - peso (inclinacao da reta)
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :return: float - o erro quadratico medio
     """
-
-
-    raise NotImplementedError  # substituir pelo seu codigo
-
+    total_error = np.mean((data[:, 1] - (w * data[:, 0] + b)) ** 2)
+    return total_error
 
 def step_gradient(b, w, data, alpha):
     """
-    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de b e w.
+    Executa uma atualização por descida do gradiente e retorna os valores atualizados de b e w.
     :param b: float - bias (intercepto da reta)
     :param w: float - peso (inclinacao da reta)
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de b e w, respectivamente
     """
-
-
-
-    raise NotImplementedError  # substituir pelo seu codigo
-
+    N = len(data)
+    b_gradient = -2 * np.sum(data[:, 1] - (w * data[:, 0] + b)) / N
+    w_gradient = -2 * np.sum((data[:, 1] - (w * data[:, 0] + b)) * data[:, 0]) / N
+    new_b = b - alpha * b_gradient
+    new_w = w - alpha * w_gradient
+    return new_b, new_w
 
 def fit(data, b, w, alpha, num_iterations):
     """
@@ -44,4 +42,9 @@ def fit(data, b, w, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    b_values, w_values = [b], [w]
+    for _ in range(num_iterations):
+        b, w = step_gradient(b, w, data, alpha)
+        b_values.append(b)
+        w_values.append(w)
+    return b_values, w_values
