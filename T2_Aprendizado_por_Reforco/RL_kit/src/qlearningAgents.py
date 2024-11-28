4624e2c9c7315c801a51af872ee2b49010bcf103
GuiDavilaP <guilhermedavilap@gmail.com>
2024-11-28 20:47:55 -0300

    Update qlearningAgents.py

--- T2_Aprendizado_por_Reforco/RL_kit/src/qlearningAgents.py
+++ T2_Aprendizado_por_Reforco/RL_kit/src/qlearningAgents.py
@@ -197,11 +197,8 @@ class ApproximateQAgent(PacmanQAgent):
 
     def final(self, state):
         "Called at the end of each game."
-        # call the super-class final method
         PacmanQAgent.final(self, state)
 
-        # did we finish training?
         if self.episodesSoFar == self.numTraining:
-            # you might want to print your weights here for debugging
             print("Weights after training:", self.weights)
             pass
